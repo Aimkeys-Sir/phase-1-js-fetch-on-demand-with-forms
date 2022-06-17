2dead5cf86a025c693e580e219fb81efe7f7e14c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-17 07:53:52 +0300

    commit tree cc0eff0bfd549617a094e8161ace83c785b6737f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const form = document.querySelector("form")
+   
+  const data= form.addEventListener('submit', (e)=> {
+        e.preventDefault()
+        let input = e.target.children[1].value
+        const section=document.getElementById("movieDetails")
+        fetch('http://localhost:3000/movies')
+           .then(response=>response.json())
+            .then(json=>{
+                json.find(movieObj=>{
+                    if(movieObj.id==input)
+                    {
+                        section.querySelector("h4").textContent=movieObj.title
+                        section.querySelector("p").textContent=movieObj.summary
+                    }
+                })
+            })
+    })
+ 
 }
 
 document.addEventListener('DOMContentLoaded', init);
